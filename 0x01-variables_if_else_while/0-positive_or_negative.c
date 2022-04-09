1e166bdf3099cd7ae8e67697262d2ac6ee75a26a
root <sanya4reel@gmail.com>
2022-04-08 18:25:51 -0700

    first C language

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main _ print a random number 
+ * and state weather it positive or nrgative
+ */
+int main (void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 1)
+printf("%d is positive\n", n);
+else if (n<0)
+printf("%d is negative\n, n);
+else
+
+printf("%d is zero\n", n);
+
+return (0);
+}
