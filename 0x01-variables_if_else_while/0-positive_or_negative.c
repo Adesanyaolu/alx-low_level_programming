9bf0644f40fda86ec4ce6486998bb9e336d25bcd
root <sanya4reel@gmail.com>
2022-04-10 17:27:18 -0700

    task 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *Main-print a random number and state weather
- * it positive, negative, or zero.
+ *main - entry point
+ * Description - print a random number and state weather
+ *	it positive, negative, or zero.
  *
  *Return: Always 0.
- */
+*/
 int main(void)
 {
 int n;
