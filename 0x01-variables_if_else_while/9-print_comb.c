786ae9d31c9a8242a62a20276e4deec5d1f7ad95
root <sanya4reel@gmail.com>
2022-04-11 17:51:17 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints all single digits numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Retunr: Always 0.
+ */
+int main(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
