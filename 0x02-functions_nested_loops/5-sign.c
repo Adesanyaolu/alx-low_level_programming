910e9ece1e66f6117c3ae4891c1c1847bb63b645
root <sanya4reel@gmail.com>
2022-04-12 18:30:23 -0700

    TASK 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,20 @@
  *	0 if the number is zero,
  *	-1 if the number is less than zero.
  */
-int print_sign(int)
+int print_sign(int n)
 {
 if (n > 0)
+{
 _putchar('+');
 return (1);
 }
 else if (n == 0)
-}
+{
 _putchar('0');
 return (0);
 }
 else
-}
+{
 _putchar('-');
 return (-1);
 }
