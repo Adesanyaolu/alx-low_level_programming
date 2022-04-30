7761bd1f771475c835198bd7895c94dece7e1494
AdeSanyaOlu <sanya4reel@gmail.com>
2022-04-30 12:58:28 +0100

    Task 0 chapter 8

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,13 @@
  * Return: Always 0.
  */
 
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
+
+void _puts_recursion(char *s){
+    if (*s == '\0'){
+        _putchar('\n');
+    } else {
+        _putchar(*s);
+        _puts_recursion(s+1);
+    };
 }
+
