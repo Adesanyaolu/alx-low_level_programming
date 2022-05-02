2dd7bdbde997751a64b9d361ff33ab38e3628442
AdeSanyaOlu <sanya4reel@gmail.com>
2022-05-02 21:29:50 +0100

    Creating a static library

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - a program that print its name
+ * followed by a new line
+ * return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	*argv[] __attribute__((unused));
+	return (0);
+}
