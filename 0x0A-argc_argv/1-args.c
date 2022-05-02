65e81bb58aeaf3c2405a2661efd2d72be919a940
root <sanya4reel@gmail.com>
2022-05-02 16:42:34 -0700

    print argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
+/**
  * main - a program that print its name
  * followed by a new line
- * return 0
+ * @argc: argument count
+ * @argv: argument variable
+ *
+ * Return: 0(succes)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc);
-	*argv[] __attribute__((unused));
 	return (0);
 }
