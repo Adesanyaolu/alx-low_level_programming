dc92cade51f13cd11a0ebe7e10f823a17c8ee3d8
AdeSanyaOlu <sanya4reel@gmail.com>
2022-05-06 00:59:18 +0100

    Merge branch 'main' of https://github.com/Adesanyaolu/alx-low_level_programming into main
    Conflicts:
    	0x0A-argc_argv/0-whatsmyname.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: the character to print
+ * @c: the character to print
+ * Return: pointer to array or null
+ * 
+ */
+char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c){
+    char *q;
+    unsigned int i;
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    q = malloc(sizeof (char) * size);
+
+    if (q == NULL)
+    {
+        return (NULL);
+    }
+    else
+    {
+        for (i = 0; i < size; i++)
+        {
+            q[i] = c;
+        }
+    }
+    return q;
+}
