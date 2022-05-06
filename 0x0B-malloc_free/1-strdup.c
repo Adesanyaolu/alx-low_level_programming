b8c3100e6e9e65f2c6aea86791310d217eb7aaeb
root <sanya4reel@gmail.com>
2022-05-06 16:27:29 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 
 /**
+ * _strdup - returns a pointer to a newly allocated space with copy of arg
  *
- *_strdup - returns a pointer to a newly allocated space with copy of arg
+ * @str: string to copy
  *
- *@str: string to copy
- *
- *Return: pointer to array or null
- *
+ * Return: pointer to array or null
  */
 
 char *_strdup(char *str)
@@ -16,199 +14,24 @@ char *_strdup(char *str)
 	char *r;
 	int i, j;
 	int coun = 0;
+
 	if (str == NULL)
 	{
-
-										return (NULL);
-
-											}
-
-
-
-						for (i = 0; str[i] != '\0'; i++)
-
-								{
-
-											coun = coun + 1;
-
-												}
-
-
-
-							r = malloc(sizeof(char) * coun + 1);
-
-
-
-								if (r == NULL)
-
-										{
-
-													return (NULL);
-
-														}
-
-									for (j = 0; str[j] != '\0' ; j++)
-
-											{
-
-														r[j] = str[j];
-
-															}
-
-
-
-										r[j] = '\0';
-
-
-
-											return (r);
-
-}#include "holberton.h"
-
-#include <stdlib.h>
-
-/**
- *
- *  * *_strdup - returns a pointer to a newly allocated space with copy of arg
- *
- *   * @str: string to copy
- *
- *    * Return: pointer to array or null
- *
- *     **/
-
-char *_strdup(char *str)
-
-{
-
-		char *r;
-
-			int i, j;
-
-				int coun = 0;
-
-
-
-					if (str == NULL)
-
-							{
-
-										return (NULL);
-
-											}
-
-
-
-						for (i = 0; str[i] != '\0'; i++)
-
-								{
-
-											coun = coun + 1;
-
-												}
-
-
-
-							r = malloc(sizeof(char) * coun + 1);
-
-
-
-								if (r == NULL)
-
-										{
-
-													return (NULL);
-
-														}
-
-									for (j = 0; str[j] != '\0' ; j++)
-
-											{
-
-														r[j] = str[j];
-
-															}
-
-
-
-										r[j] = '\0';
-
-
-
-											return (r);
-
-}i#include "holberton.h"
-
-#include <stdlib.h>
-
-/**
- *
- *  * *_strdup - returns a pointer to a newly allocated space with copy of arg
- *
- *   * @str: string to copy
- *
- *    * Return: pointer to array or null
- *
- *     **/
-
-char *_strdup(char *str)
-
-{
-
-		char *r;
-
-			int i, j;
-
-				int coun = 0;
-
-
-
-					if (str == NULL)
-
-							{
-
-										return (NULL);
-
-											}
-
-
-
-						for (i = 0; str[i] != '\0'; i++)
-
-								{
-
-											coun = coun + 1;
-
-												}
-
-
-
-							r = malloc(sizeof(char) * coun + 1);
-
-
-
-								if (r == NULL)
-
-										{
-
-													return (NULL);
-
-														}
-
-									for (j = 0; str[j] != '\0' ; j++)
-
-											{
-
-														r[j] = str[j];
-
-															}
-
-
-
-										r[j] = '\0';
-
-
-
-											return (r);
-
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		coun = coun + 1;
+	}
+	r = malloc(sizeof(char) * coun + 1);
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0' ; j++)
+	{
+		r[j] = str[j];
+	}
+	r[j] = '\0';
+	return (r);
 }
