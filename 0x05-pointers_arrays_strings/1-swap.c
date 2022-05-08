f42ba957135e64ab4405b6d4e39e95d7f76d0c40
root <sanya4reel@gmail.com>
2022-05-08 13:09:41 -0700

    Pointers task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,21 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * swap_int - Check if a number is greater than 0
+ * @a: The number to be checked
+ * @b: Second operand
  */
-void swap_int(int *a, int *b);
-int main(void)
-{
-    int a;
-    int b;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+void swap_int(int *a, int *b)
+
+{
+	int aux;
+
+	aux = *b;
+	*b = *a;
+	*a = aux;
 }
