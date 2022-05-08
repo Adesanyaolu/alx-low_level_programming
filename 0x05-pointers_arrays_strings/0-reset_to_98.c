62a15e811d85f57a6982384581673f4a6fabf434
root <sanya4reel@gmail.com>
2022-05-08 12:56:20 -0700

    pointers task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
-
 /**
- * main - derefencing pointers
- *
- * Return: Always 0.
+ * reset_to_98 - This is a description
+ * @n: The number to be checked
  */
 
+void reset_to_98(int *n)
 
-int main(void)
 {
-   int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-
-   
-    return (0);
-
-
-
+	*n = 98;
 }
