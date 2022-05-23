49e01c46e6bad66d5bb61b6433642ae72fce98b3
root <sanya4reel@gmail.com>
2022-05-22 17:44:56 -0700

    common predefined macros

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
-/*
- * main - prints the name of the file it was compiled from
+/**
+ * main - print the name of the file it was compiled from.
  *
- * Return always (0)
- *
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	printf("%s\n", __BASE_FILE__);
